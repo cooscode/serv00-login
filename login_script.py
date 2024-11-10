bb6af331ae2acb710e173392a21568d012a10f31
Malcolm Maxwell <167549703+cooscode@users.noreply.github.com>
2024-11-10 20:48:00 +0800

    Update script

--- login_script.py
+++ login_script.py
@@ -59,7 +59,7 @@ async def login(username, password, panel):
         return is_logged_in
 
     except Exception as e:
-        print(f'{serviceName}账号 {username} 登录时出现错误: {e}')
+        print(f'账号登录时出现错误: {e}')
         return False
 
     finally:
@@ -89,12 +89,12 @@ async def main():
         if is_logged_in:
             now_utc = format_to_iso(datetime.utcnow())
             now_beijing = format_to_iso(datetime.utcnow() + timedelta(hours=8))
-            success_message = f'{serviceName}账号 {username} 于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
+            success_message = f'账号于北京时间 {now_beijing}（UTC时间 {now_utc}）登录成功！'
             message += success_message + '\n'
             print(success_message)
         else:
-            message += f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。\n'
-            print(f'{serviceName}账号 {username} 登录失败，请检查{serviceName}账号和密码是否正确。')
+            message += f'账号登录失败，请检查账号和密码是否正确。\n'
+            print(f'账号登录失败，请检查账号和密码是否正确。')
 
         delay = random.randint(1000, 8000)
         await delay_time(delay)
